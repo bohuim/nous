b894600485e35145521891a92e7171c269c5416a
Bohui Moon <bohuimoon@gmail.com>
2017-04-20 09:46:17 -0700

    Web-Alexa pubsub test of concept

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
 import React from 'react'
-import logo from './logo.svg'
+import PubNub from 'pubnub'
 import './App.css'
+
 
 class App extends React.Component
 {
+    constructor(props)
+    {
+        super(props)
+
+        this.state = {
+            isConnected: 'waiting...'
+        }
+
+        this.pubnub = new PubNub({
+            publishKey: 'pub-c-8fd5e639-8131-4b76-867c-c38d0c1d15fc',
+            subscribeKey: 'sub-c-7eaa1852-2563-11e7-bb8a-0619f8945a4f'
+        })
+    }
+
+    componentDidMount()
+    {
+        this.pubnub.addListener({
+            message: this.messageHandler,
+            status: this.connectHandler
+        })
+
+        this.pubnub.subscribe({
+            channels: ['nous-alexa']
+        })
+    }
+
     render()
     {
         return (
             <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
-
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <p>Connect status: {this.state.isConnected}</p>
             </div>
         )
     }
+
+
+    // --------------------
+    // PubNub
+    // --------------------
+    connectHandler = (event) =>
+    {
+        const category = event.category
+        if (category === 'PNUnknownStatus')
+        {
+            this.setState({isConnected: 'error on subscription'})
+            return
+        }
+
+        this.setState({isConnected: 'subscribed!'})
+        return  
+    };
+
+    messageHandler = (message) =>
+    {
+        console.log(message)
+    };
 }
 
 export default App
