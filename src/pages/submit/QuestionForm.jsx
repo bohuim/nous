1f895b2195eb0b75373fdc9008743068d0b80e8b
Bohui Moon <bohuimoon@gmail.com>
2017-04-21 14:34:00 -0700

    basic QuestionForm styling

--- src/pages/submit/QuestionForm.jsx
+++ src/pages/submit/QuestionForm.jsx
@@ -2,16 +2,35 @@ import React from 'react'
 
 import 'styles/QuestionForm'
 
+export default
 class QuestionForm extends React.Component
 {
+    placeholder = 'Type the question here...'
+
     render()
     {
+        const name = 'question'
+
         return (
             <div className='QuestionForm'>
-                
+                <div className='toolbar'>
+                    <p className='title'>Question #1</p>
+                    <i className='material-icons'>clear</i>
+                </div>
+
+                <textarea 
+                    name={name}
+                    className='input'
+                    placeholder={this.placeholder}
+                    rows="8" cols="48"
+                ></textarea>
+
+                <input 
+                    type='text' 
+                    className='input'
+                    placeholder='Comma separated categories ex) general, chemistry, ...'
+                />
             </div>
         )
     }
 }
-
-export default QuestionForm
--- src/pages/submit/SubmitPage.jsx
+++ src/pages/submit/SubmitPage.jsx
@@ -3,16 +3,30 @@ import React from 'react'
 import 'styles/SubmitPage'
 import QuestionForm from './QuestionForm'
 
+export default
 class SubmitPage extends React.Component
 {
+    constructor(props)
+    {
+        super(props)
+
+        this.state = {
+            count: 1
+        }
+    }
+
     render()
     {
         return (
-            <div className='SubmitPage'>
-                
+            <div className='SubmitPage page'>
+                <div className='content'>
+                    <h3>Submit New Questions</h3>
+                    <QuestionForm />
+                </div>           
             </div>
         )
     }
 }
 
-export default SubmitPage
+// Helpers
+const range = (n) => Array.from(Array(n).keys())
