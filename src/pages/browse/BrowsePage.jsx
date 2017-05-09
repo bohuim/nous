089c15aa21bfdf33f384450bc2f11ec08b3658ef
Bohui Moon <bohuimoon@gmail.com>
2017-05-09 10:56:59 -0700

    browse page

--- src/pages/browse/BrowsePage.jsx
+++ src/pages/browse/BrowsePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
+import request from 'superagent'
 import pubnub from '~/utils/pubnub'
 
 import './BrowsePage.scss'
@@ -7,20 +9,55 @@ import './BrowsePage.scss'
 export default
 class BrowsePage extends React.Component
 {
+    constructor(props)
+    {
+        super(props)
+
+        this.state = {
+            search: '',
+            categories: []
+        }
+    }
+
+    componentWillMount()
+    {
+        request
+        .get('https://y7sn9xsm9h.execute-api.us-east-1.amazonaws.com/prod/NousDB')
+        .query({TableName: 'NousCategories'})
+        .end((error, response) => {
+            if (error)
+                return console.log('BrowsePage.componentWillMount() - error while fetching categories: ', error)
+
+            const categories = 
+                response.body.Items
+                .map(item => ({category: item.Category, count: item.Count}))
+
+            this.setState({categories: categories})
+        })
+    }
+
     render()
     {
-        const category = (this.props.match.params.category || '').toLowerCase()
-        console.log('category: ', category)
+        const items = 
+            this.state.categories
+            .filter(item => item.category.includes(this.state.search))
+            .map(item => (
+                <li key={item.category} styleName='item'>
+                <Link to={'/browse/' + item.category} styleName='item-link'>
+                    <span styleName='category'>{'#' + item.category}</span>
+                    <span styleName='count'>{`${item.count} questions`}</span>
+                </Link>
+                </li>
+            ))
 
         return (
             <div styleName='page'>
-                <div styleName='toolbar'>
+                <input styleName='searchbar'
+                       value={this.state.search}
+                       placeholder='search for a category...'
+                       onChange={e => this.setState({search: e.target.value})} />
 
-                </div>
-
-                <ul styleName='content'>
-
-                </ul>
+                <ul styleName='content'>{items}</ul>
             </div>
         )
     }
